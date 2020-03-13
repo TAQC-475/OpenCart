6d12be3f20b05fbd2e966dedf13b9df8c7e01be7
BorisTitarenko <specmohnat2309@gmail.com>
2020-03-14 00:07:18 +0200

    Fix class names

--- src/test/java/com/softserve/edu/opencart/pages/emailclient/unlogged/EmailLoginPage.java
+++ src/test/java/com/softserve/edu/opencart/pages/emailclient/unlogged/EmailLoginPage.java
@@ -1,5 +1,5 @@
 package com.softserve.edu.opencart.pages.emailclient.unlogged;
 
-public class EmailLoginPage extends ServicesNavigatePart{
+public class EmailLoginPage extends PortalServicesPart{
     private EmailLoginFormPart emailLoginPart;
 }
