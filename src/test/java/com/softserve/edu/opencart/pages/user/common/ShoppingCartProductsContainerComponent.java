2c7c1b9a57a8799705f80cd16b35af7f8edf7cdd
Pavlo Tytarchuk <tytarchuk.pavel@gmail.com>
2020-03-15 15:28:52 +0200

    added new  methods to ShoppingCartProductsContainerComponent class

--- src/test/java/com/softserve/edu/opencart/pages/user/common/ShoppingCartProductsContainerComponent.java
+++ src/test/java/com/softserve/edu/opencart/pages/user/common/ShoppingCartProductsContainerComponent.java
@@ -1,5 +1,6 @@
 package com.softserve.edu.opencart.pages.user.common;
 
+import com.softserve.edu.opencart.data.Product;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -32,5 +33,36 @@ public class ShoppingCartProductsContainerComponent {
 
     public int getShoppingCartProductComponentCount(){
         return getShoppingCartProductComponents().size();
+    }
+
+    public List<String> getShoppingCartProductComponentNames() {
+        List<String> shoppingCartProductComponentNames = new ArrayList<>();
+        for (ShoppingCartProductComponent current : getShoppingCartProductComponents())
+        {
+            shoppingCartProductComponentNames.add(current.getProductNameText());
+        }
+        return shoppingCartProductComponentNames;
+    }
+
+    protected ShoppingCartProductComponent getShoppingCartProductComponentByName(String productName){
+        ShoppingCartProductComponent result = null;
+        for(ShoppingCartProductComponent current: getShoppingCartProductComponents()){
+            if(current.getProductNameText().toLowerCase().equals(productName.toLowerCase())){
+                result = current;
+                break;
+            }
+        }
+        if(result == null){
+            throw new RuntimeException(String.format("Product with name: %s not found", productName));
+        }
+        return result;
+    }
+
+    public String getShoppingCartProductUnitPriceByName(String productName){
+        return getShoppingCartProductComponentByName(productName).getUnitPriceText();
+    }
+
+    public ShoppingCartProductComponent getShoppingCartProductComponentByName(Product product){
+        return getShoppingCartProductComponentByName(product.getName());
     }
 }
