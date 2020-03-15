c47298cd4b0acc39c6c3b0b631791b268da4bbc9
Boris <borismenko.sgmail.com>
2020-03-15 09:46:51 +0200

    added Regular Expresion class

---
+++ src/test/java/com/softserve/edu/opencart/tools/RegularExpression.java
@@ -0,0 +1,19 @@
+package com.softserve.edu.opencart.tools;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public final class RegularExpression {
+
+  public RegularExpression() {
+  }
+
+  public int getNumberFromWishList(String str){
+    int listNumber = 0;
+    Pattern regularExpressionPattern = Pattern.compile("-?\\d+");
+    Matcher matcher = regularExpressionPattern.matcher(str);
+    while (matcher.find()){
+      listNumber = Integer.parseInt(matcher.group());}
+    return listNumber;
+  }
+}
