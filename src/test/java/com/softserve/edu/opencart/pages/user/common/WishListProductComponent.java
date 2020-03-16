b7ff4e8f5de2a5fdf4c4a119b98e53ef395391c7
Boris <borismenko.sgmail.com>
2020-03-16 11:07:08 +0200

    upade WishListProductComponent

--- src/test/java/com/softserve/edu/opencart/pages/user/common/WishListProductComponent.java
+++ src/test/java/com/softserve/edu/opencart/pages/user/common/WishListProductComponent.java
@@ -1,5 +1,70 @@
 package com.softserve.edu.opencart.pages.user.common;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
 public final class WishListProductComponent {
 
+    private WebElement tableLayout;
+    private WebElement image;
+    private WebElement name;
+    private WebElement model;
+    private WebElement stock;
+    private WebElement price;
+    private WebElement addToCartButton;
+    private WebElement removeFromWIshListButton;
+
+
+  public WishListProductComponent(WebElement tableLayout) {
+    this.tableLayout = tableLayout;
+    initElements();
+  }
+
+  private void initElements(){
+    image = tableLayout.findElement(By.xpath("//div[@class='table-responsive']//td[@class='text-center']/a"));
+    name = tableLayout.findElement(By.xpath("//div[@class='table-responsive']//td[@class='text-left']/a"));
+    model = tableLayout.findElement(By.xpath("//tbody//td[contains(text(),'Product')]"));
+    stock = tableLayout.findElement(By.xpath("//tbody//td[contains(text(),'Stock')]"));
+    price = tableLayout.findElement(By.xpath("//div[@class='price']"));
+    addToCartButton = tableLayout.findElement(By.xpath("//div//button[@data-original-title='Add to Cart']"));
+    removeFromWIshListButton = tableLayout.findElement(By.xpath("//div//a[@data-original-title='Remove']"));
+
+  }
+
+  public WebElement getImage() {
+    return image;
+  }
+
+  public WebElement getName() {
+    return name;
+  }
+
+  public WebElement getModel() {
+    return model;
+  }
+
+  public WebElement getStock() {
+    return stock;
+  }
+
+  public WebElement getPrice() {
+    return price;
+  }
+
+  public WebElement getAddToCartButton() {
+    return addToCartButton;
+  }
+  public void clickAddToCartButton() {
+    getAddToCartButton().click();
+  }
+
+  public WebElement getRemoveFromWIshListButton() {
+    return removeFromWIshListButton;
+  }
+
+  public void clickRemoveFromWIshListButton() {
+    getRemoveFromWIshListButton().click();
+  }
 }
+
+
--- src/test/java/com/softserve/edu/opencart/pages/user/common/WishListProductsContainerComponent.java
+++ src/test/java/com/softserve/edu/opencart/pages/user/common/WishListProductsContainerComponent.java
@@ -1,9 +1,20 @@
 package com.softserve.edu.opencart.pages.user.common;
 
 import java.util.List;
+import org.openqa.selenium.WebDriver;
 
 public class WishListProductsContainerComponent {
+  private final String PRODUCT_COMPONENT_XPATH ="//div[@class='table-responsive']//tbody//tr";
+
+  protected WebDriver webDriver;
 
   private List<WishListProductComponent> productComponents;
 
+  public WishListProductsContainerComponent(WebDriver webDriver) {
+    this.webDriver = webDriver;
+    initElements();
+  }
+  private void initElements(){
+
+  }
 }
