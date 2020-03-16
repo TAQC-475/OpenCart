52a555119321747be0c2afa939b32d0585f9800f
Boris <borismenko.sgmail.com>
2020-03-16 12:28:55 +0200

    update WishListProductcomponent and WishListCOnteinerComponent

--- src/test/java/com/softserve/edu/opencart/pages/user/common/WishListProductComponent.java
+++ src/test/java/com/softserve/edu/opencart/pages/user/common/WishListProductComponent.java
@@ -1,18 +1,19 @@
 package com.softserve.edu.opencart.pages.user.common;
 
+import com.softserve.edu.opencart.pages.user.search.ProductInfoPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 public final class WishListProductComponent {
 
-    private WebElement tableLayout;
-    private WebElement image;
-    private WebElement name;
-    private WebElement model;
-    private WebElement stock;
-    private WebElement price;
-    private WebElement addToCartButton;
-    private WebElement removeFromWIshListButton;
+  private WebElement tableLayout;
+  private WebElement image;
+  private WebElement name;
+  private WebElement model;
+  private WebElement stock;
+  private WebElement price;
+  private WebElement addToCartButton;
+  private WebElement removeFromWIshListButton;
 
 
   public WishListProductComponent(WebElement tableLayout) {
@@ -20,14 +21,18 @@ public final class WishListProductComponent {
     initElements();
   }
 
-  private void initElements(){
-    image = tableLayout.findElement(By.xpath("//div[@class='table-responsive']//td[@class='text-center']/a"));
-    name = tableLayout.findElement(By.xpath("//div[@class='table-responsive']//td[@class='text-left']/a"));
+  private void initElements() {
+    image = tableLayout
+        .findElement(By.xpath("//div[@class='table-responsive']//td[@class='text-center']/a"));
+    name = tableLayout
+        .findElement(By.xpath("//div[@class='table-responsive']//td[@class='text-left']/a"));
     model = tableLayout.findElement(By.xpath("//tbody//td[contains(text(),'Product')]"));
     stock = tableLayout.findElement(By.xpath("//tbody//td[contains(text(),'Stock')]"));
     price = tableLayout.findElement(By.xpath("//div[@class='price']"));
-    addToCartButton = tableLayout.findElement(By.xpath("//div//button[@data-original-title='Add to Cart']"));
-    removeFromWIshListButton = tableLayout.findElement(By.xpath("//div//a[@data-original-title='Remove']"));
+    addToCartButton = tableLayout
+        .findElement(By.xpath("//div//button[@data-original-title='Add to Cart']"));
+    removeFromWIshListButton = tableLayout
+        .findElement(By.xpath("//div//a[@data-original-title='Remove']"));
 
   }
 
@@ -39,8 +44,21 @@ public final class WishListProductComponent {
     return name;
   }
 
+  public void clickProductName(){
+    getName().click();
+  }
+
+  public String getNameText() {
+
+    return getName().getText();
+  }
+
   public WebElement getModel() {
     return model;
+  }
+
+  public String getModelText(){
+     return getModel().getText();
   }
 
   public WebElement getStock() {
@@ -50,10 +68,15 @@ public final class WishListProductComponent {
   public WebElement getPrice() {
     return price;
   }
+  // TODO when there two prices
+  public String getPriceText(){
+    return getPrice().getText();
+  }
 
   public WebElement getAddToCartButton() {
     return addToCartButton;
   }
+
   public void clickAddToCartButton() {
     getAddToCartButton().click();
   }
--- src/test/java/com/softserve/edu/opencart/pages/user/common/WishListProductsContainerComponent.java
+++ src/test/java/com/softserve/edu/opencart/pages/user/common/WishListProductsContainerComponent.java
@@ -1,20 +1,78 @@
 package com.softserve.edu.opencart.pages.user.common;
 
+import java.util.ArrayList;
 import java.util.List;
+import com.softserve.edu.opencart.data.Product;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class WishListProductsContainerComponent {
   private final String PRODUCT_COMPONENT_XPATH ="//div[@class='table-responsive']//tbody//tr";
 
-  protected WebDriver webDriver;
+  protected WebDriver driver;
 
-  private List<WishListProductComponent> productComponents;
+  private List<WishListProductComponent> wishListComponents;
 
   public WishListProductsContainerComponent(WebDriver webDriver) {
-    this.webDriver = webDriver;
+    this.driver = webDriver;
     initElements();
   }
   private void initElements(){
-
+  wishListComponents = new ArrayList<>();
+  for (WebElement current: driver.findElements(By.xpath(PRODUCT_COMPONENT_XPATH))){
+    wishListComponents.add(new WishListProductComponent(current));
   }
+  }
+
+  public List<WishListProductComponent> getWishListComponents() {
+    return wishListComponents;
+  }
+
+  public int getWishListComponentsCount(){
+    return wishListComponents.size();
+  }
+
+  public List<String> getWishListComponentNames(){
+    List<String> wishListComponentNames = new ArrayList<>();
+    for (WishListProductComponent current: getWishListComponents()){
+      wishListComponentNames.add(current.getNameText());
+    }
+    return wishListComponentNames;
+  }
+
+  protected WishListProductComponent getWishListComponentByName(String productName){
+    WishListProductComponent result = null;
+    for (WishListProductComponent current:getWishListComponents()){
+      if (current.getNameText().toLowerCase().equals(productName.toLowerCase())){
+        result = current;
+        break;
+      }
+    }
+    if (result == null){
+      throw new RuntimeException("ProductName: " + productName + " not Found.");
+    }
+    return result;
+  }
+
+  public String getWishListComponentPriceByName(String productName){
+    return getWishListComponentByName(productName).getPriceText();
+  }
+
+  public String getWishListComponentModelByName(String productName){
+    return getWishListComponentByName(productName).getModelText();
+  }
+
+  public void clickWishListComponentAddToCartButtonByName(String productName){
+    getWishListComponentByName(productName).clickAddToCartButton();
+  }
+
+  public void clickWishListComponentRemoveButtonByName(String productName){
+    getWishListComponentByName(productName).clickRemoveFromWIshListButton();
+  }
+
+  public void clickWishListComponentNameByName(String productName){
+    getWishListComponentByName(productName).clickProductName();
+  }
+
 }
