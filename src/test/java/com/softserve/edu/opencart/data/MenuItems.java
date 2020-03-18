2298f456a790867f8cf266bfc7cc291e7f70c514
stadnytskyivp <stadnytskyi.volodymyr@gmail.com>
2020-03-18 23:41:08 +0200

    changes in menu items

--- src/test/java/com/softserve/edu/opencart/data/MenuItems.java
+++ src/test/java/com/softserve/edu/opencart/data/MenuItems.java
@@ -1,37 +1,96 @@
 package com.softserve.edu.opencart.data;
 
-public enum MenuItems {
+public class MenuItems {
 
-    DESKTOPS("Desktops"),
-    PC("PC"),
-    MAC("Mac"),
+    private String desktops;
+    private String laptopsNotebooks;
+    private String components;
+    private String tablets;
+    private String software;
+    private String phonesPDAs;
+    private String cameras;
+    private String mp3Players;
 
-    LAPTOPS_AND_NOTEBOOKS("Laptops & Notebooks"),
-    MACS("Macs"),
-    WINDOWS("Windows"),
-
-    COMPONENTS("Components"),
-    MICE_AND_TRACKBALLS("Mice and Trackballs"),
-    MONITORS("Monitors"),
-    PRINTERS("Printers"),
-    SCANNERS("Scanners"),
-    WEB_CAMERAS("Web Cameras"),
-
-    TABLETS("Tablets"),
-    SOFTWARE("Software"),
-    PHONES_AND_PDAS("Phones & PDAs"),
-    CAMERAS("Cameras"),
-    MP3_PLAYERS("MP3 Players");
-
-    private String itemName;
-
-    private MenuItems(String itemName) {
-
-        this.itemName = itemName;
+    public MenuItems(String desktops, String laptopsNotebooks, String components, String tablets, String software, String phonesPDAs, String cameras, String mp3Players) {
+        this.desktops = desktops;
+        this.laptopsNotebooks = laptopsNotebooks;
+        this.components = components;
+        this.tablets = tablets;
+        this.software = software;
+        this.phonesPDAs = phonesPDAs;
+        this.cameras = cameras;
+        this.mp3Players = mp3Players;
     }
 
-    @Override
-    public String toString() {
-        return itemName;
+    public String getDesktops() {
+        return desktops;
+    }
+
+    public void setDesktops(String desktops) {
+        this.desktops = desktops;
+    }
+
+    public String getLaptopsNotebooks() {
+        return laptopsNotebooks;
+    }
+
+    public void setLaptopsNotebooks(String laptopsNotebooks) {
+        this.laptopsNotebooks = laptopsNotebooks;
+    }
+
+    public String getComponents() {
+        return components;
+    }
+
+    public void setComponents(String components) {
+        this.components = components;
+    }
+
+    public String getTablets() {
+        return tablets;
+    }
+
+    public void setTablets(String tablets) {
+        this.tablets = tablets;
+    }
+
+    public String getSoftware() {
+        return software;
+    }
+
+    public void setSoftware(String software) {
+        this.software = software;
+    }
+
+    public String getPhonesPDAs() {
+        return phonesPDAs;
+    }
+
+    public void setPhonesPDAs(String phonesPDAs) {
+        this.phonesPDAs = phonesPDAs;
+    }
+
+    public String getCameras() {
+        return cameras;
+    }
+
+    public void setCameras(String cameras) {
+        this.cameras = cameras;
+    }
+
+    public String getMp3Players() {
+        return mp3Players;
+    }
+
+    public void setMp3Players(String mp3Players) {
+        this.mp3Players = mp3Players;
     }
 }
+//    DESKTOPS("Desktops"),
+//    LAPTOPS_AND_NOTEBOOKS("Laptops & Notebooks"),
+//    COMPONENTS("Components"),
+//    TABLETS("Tablets"),
+//    SOFTWARE("Software"),
+//    PHONES_AND_PDAS("Phones & PDAs"),
+//    CAMERAS("Cameras"),
+//    MP3_PLAYERS("MP3 Players");
--- src/test/java/com/softserve/edu/opencart/pages/user/search/ProductsSidebarFullPage.java
+++ src/test/java/com/softserve/edu/opencart/pages/user/search/ProductsSidebarFullPage.java
@@ -1,8 +1,12 @@
 package com.softserve.edu.opencart.pages.user.search;
 
+import com.softserve.edu.opencart.pages.user.common.DropdownComponent;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class ProductsSidebarFullPage extends ProductsSidebarPart {
+
+	private ProductsDisplayComponent productsDisplayComponent;
 
 	public ProductsSidebarFullPage(WebDriver driver) {
 		super(driver);
@@ -11,6 +15,29 @@ public class ProductsSidebarFullPage extends ProductsSidebarPart {
 
 	private void initElements() {
 		// init elements
+		productsDisplayComponent = new ProductsDisplayComponent(driver);
+
+//				DESKTOPS("Desktops"),
+//				PC("PC"),
+//				MAC("Mac"),
+//
+//				LAPTOPS_AND_NOTEBOOKS("Laptops & Notebooks"),
+//				MACS("Macs"),
+//				WINDOWS("Windows"),
+//
+//				COMPONENTS("Components"),
+//				MICE_AND_TRACKBALLS("Mice and Trackballs"),
+//				MONITORS("Monitors"),
+//				PRINTERS("Printers"),
+//				SCANNERS("Scanners"),
+//				WEB_CAMERAS("Web Cameras"),
+//
+//				TABLETS("Tablets"),
+//				SOFTWARE("Software"),
+//				PHONES_AND_PDAS("Phones & PDAs"),
+//				CAMERAS("Cameras"),
+//				MP3_PLAYERS("MP3 Players");
+
 	}
 
 	// Page Object
