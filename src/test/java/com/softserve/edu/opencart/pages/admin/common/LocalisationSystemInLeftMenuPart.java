2c0ad85ce31883c2617c4a618d1cfa260d38e071
LyubomyrKovch <lkovch@gmail.com>
2020-03-18 19:39:35 +0200

    add two new classes which extends LeftMenuPart in admin page and new web elements

--- src/test/java/com/softserve/edu/opencart/pages/admin/common/LocalisationSystemInLeftMenuPart.java
+++ src/test/java/com/softserve/edu/opencart/pages/admin/common/LocalisationSystemInLeftMenuPart.java
@@ -6,12 +6,19 @@ import org.openqa.selenium.WebElement;
 
 public class LocalisationSystemInLeftMenuPart extends SystemInLeftMenuPart {
 
+//    private WebElement localisationOpen;
+    private WebElement storeLocation;
+    private WebElement languages;
+    private WebElement currencies;
 
     public LocalisationSystemInLeftMenuPart(WebDriver driver) {
         super(driver);
         initElements();
     }
     private void initElements() {
-
+//        localisationOpen = driver.findElement(By.cssSelector("#menu-system > ul > li.open > a"));
+        storeLocation = driver.findElement(By.xpath("//*[@id=\"menu-system\"]//a[contains(text(), 'Store')]"));
+        languages = driver.findElement(By.xpath("//*[@id=\"menu-system\"]//a[contains(text(), 'Languages')]"));
+        currencies = driver.findElement(By.xpath("//*[@id=\"menu-system\"]//a[contains(text(), 'Currencies')]"));
     }
 }
