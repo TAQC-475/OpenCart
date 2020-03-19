2ee5f8a180d3b26e6c72fc959f21b47f7f73cd4a
stadnytskyivp <stadnytskyi.volodymyr@gmail.com>
2020-03-19 19:36:37 +0200

    added admin add categiy test

---
+++ src/test/java/com/softserve/edu/opencart/tests/AddingFromAdminTest.java
@@ -0,0 +1,35 @@
+package com.softserve.edu.opencart.tests;
+
+import com.softserve.edu.opencart.data.IUser;
+import com.softserve.edu.opencart.data.UserRepository;
+import com.softserve.edu.opencart.pages.admin.account.SigninPage;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class AddingFromAdminTest extends LocalAdminTestRunner{
+
+    @DataProvider // (parallel = true)
+    public Object[][] correctUsers() {
+        return new Object[][] {
+                { UserRepository.get().getAdmin()},
+        };
+    }
+
+    @Test(dataProvider = "correctUsers")
+    public void addNewCategory(IUser validAdmin) throws InterruptedException {
+
+        loadSigninPage()
+                .successfulLogin(validAdmin)
+                .gotoCategoriesPage()
+                .gotoAddCategoryPage()
+                .typeName("TV")
+                .typeTitle("test")
+                .clickDataButton()
+                .typeParent("test")
+                .gotoModifiedCatalogPage()
+                .getSuccessText();
+
+        Thread.sleep(3000);
+    }
+
+}
