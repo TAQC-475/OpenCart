1dc72a1042de6d1d10512488dfb51a867ad78830
LyubomyrKovch <lkovch@gmail.com>
2020-03-19 18:19:18 +0200

    in class Currency create getters frome ICurrency

--- src/test/java/com/softserve/edu/opencart/data/Currency.java
+++ src/test/java/com/softserve/edu/opencart/data/Currency.java
@@ -2,4 +2,27 @@ package com.softserve.edu.opencart.data;
 
 public final class Currency implements ICurrency {
 
+    public String getCurrencyTitleField() {
+        return null;
+    }
+
+    public String getCurrencyCodeField() {
+        return null;
+    }
+
+    public String getCurrencySymbolLeftField() {
+        return null;
+    }
+
+    public String getCurrencySymbolRightField() {
+        return null;
+    }
+
+    public String getCurrencyDecimalPlacesField() {
+        return null;
+    }
+
+    public double getCurrencyValueField() {
+        return 0;
+    }
 }
