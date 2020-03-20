567020a6fa6ba0e4d27a7b3aad04a6b8ac332264
stadnytskyivp <stadnytskyi.volodymyr@gmail.com>
2020-03-20 20:35:59 +0200

    modified LocalAdminTestRunner

--- src/test/java/com/softserve/edu/opencart/tests/LocalAdminTestRunner.java
+++ src/test/java/com/softserve/edu/opencart/tests/LocalAdminTestRunner.java
@@ -1,20 +1,39 @@
 package com.softserve.edu.opencart.tests;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.testng.ITestContext;
 import org.testng.annotations.BeforeMethod;
 
 import com.softserve.edu.opencart.pages.admin.account.SigninPage;
+import org.testng.annotations.Parameters;
+
+import java.util.Map;
 
 public abstract class LocalAdminTestRunner extends EpizyUserTestRunner {
-	private String adminServerUrl = "http://192.168.174.130/opencart/upload/admin";
-	
+	private String adminServerUrl;
+
+	@Parameters({"adminUrl"})
+	@BeforeMethod
+	public void beforeClass(ITestContext context, String adminUrl) {
+
+		for (Map.Entry<String, String> entry : context.getCurrentXmlTest().getAllParameters().entrySet()) {
+			System.out.println("Key: " + entry.getKey() + "  Value: " + entry.getValue());
+			if (entry.getKey().toLowerCase().equals("adminurl")) {
+				adminUrl = entry.getValue();
+			}
+		}
+		adminServerUrl=adminUrl;
+		WebDriverManager.chromedriver().setup();
+		getDriver().manage().window().maximize();
+	}
+
 	@BeforeMethod
 	public void beforeMethod() {
-		//driver.get(adminServerUrl);
 		getDriver().get(adminServerUrl);
 	}
 
 	public SigninPage loadSigninPage() {
-		//return new SigninPage(driver);
+		System.out.println(getDriver());
 		return new SigninPage(getDriver());
 	}
 
