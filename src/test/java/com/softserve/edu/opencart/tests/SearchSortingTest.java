68c330f0668e1f47eb7462b05a254cfb9ad45b9f
Andrew Rorat <rorat6937@gmail.com>
2020-03-21 16:31:24 +0200

    Added DataProvider to the SearchSortingTest

--- src/test/java/com/softserve/edu/opencart/tests/SearchSortingTest.java
+++ src/test/java/com/softserve/edu/opencart/tests/SearchSortingTest.java
@@ -1,46 +1,64 @@
 package com.softserve.edu.opencart.tests;
 
-import com.softserve.edu.opencart.data.Pagination;
-import com.softserve.edu.opencart.pages.user.search.SearchSuccessPage;
+import com.softserve.edu.opencart.data.CountOfProducts;
+import com.softserve.edu.opencart.data.SortByFilter;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import static com.softserve.edu.opencart.pages.user.search.SearchSuccessPage.*;
 
 public class SearchSortingTest extends SearchTestRunner {
 
-
-    @Test(description = "verify 'Sort by:' drop down menu")
-    public void checkSortByDropDownMenu() {
-        successPage()
-                .sortProductsByCriteria(SELECT_SORT_BY_FILTER);
-
-        Assert.assertTrue(SearchSuccessPage.isSortByCorrectCriteria(SELECT_SORT_BY_FILTER));
+    @DataProvider
+    public Object[][] dataForSortByFilter() {
+        return new Object[][]{{
+                SortByFilter.MODEL_AZ,
+                SortByFilter.MODEL_AZ
+        }};
     }
 
-    @Test(description = "verify 'Show:' drop down menu")
-    public void checkShowDropDownMenu() {
+    @DataProvider
+    public Object[][] dataForShowFilter() {
+        return new Object[][]{{
+                CountOfProducts.FIFTY,
+                CountOfProducts.FIFTY
+        }};
+    }
+
+    @Test(dataProvider = "dataForSortByFilter", description = "verify 'Sort by:' drop down filter")
+    public void checkSortByDropDownMenu(SortByFilter actualFilter, SortByFilter expectedFilter) {
         successPage()
-                .showProductsByCount(SELECT_SHOW_FILTER);
-        Assert.assertTrue(SearchSuccessPage.isShowCorrectQuantity(SELECT_SHOW_FILTER));
+                .sortProductsByCriteria(actualFilter);
+
+        Assert.assertTrue(isSortByCorrectCriteria(expectedFilter));
+    }
+
+    @Test(dataProvider = "dataForShowFilter", description = "verify 'Show:' drop down filter")
+    public void checkShowDropDownMenu(CountOfProducts actualInput, CountOfProducts expectedInput) {
+        successPage()
+                .showProductsByCount(actualInput);
+        Assert.assertTrue(isShowCorrectQuantity(expectedInput));
     }
 
     @Test(description = "verify that 'Grid and List' button works properly")
-    public void checkGridView() {
+    public void checkGridAndListView() {
         successPage()
                 .viewProductsByGrid();
-        softAssert.assertTrue(SearchSuccessPage.isGridViewDisplayed());
+        softAssert.assertTrue(isGridViewDisplayed());
 
         successPage()
                 .viewProductsByList();
-        softAssert.assertTrue(SearchSuccessPage.isListViewDisplayed());
+        softAssert.assertTrue(isListViewDisplayed());
 
         softAssert.assertAll();
     }
-
-    @Test(description = "verify pagination")
-    public void checkPagination() {
-        successPage()
-                .clickNeededPage(Pagination.NEXT_PAGE);
-
-
-    }
+//
+//    @Test(description = "verify pagination")
+//    public void checkPagination() {
+//        successPage()
+//                .clickNeededPage(Pagination.NEXT_PAGE);
+//
+//
+//    }
 }
--- src/test/java/com/softserve/edu/opencart/tests/SearchTestRunner.java
+++ src/test/java/com/softserve/edu/opencart/tests/SearchTestRunner.java
@@ -1,16 +1,12 @@
 package com.softserve.edu.opencart.tests;
 
-import com.softserve.edu.opencart.data.CountOfProducts;
 import com.softserve.edu.opencart.data.ProductRepository;
-import com.softserve.edu.opencart.data.SortByFilter;
 import com.softserve.edu.opencart.pages.user.search.SearchSuccessPage;
 import org.testng.annotations.BeforeMethod;
 import org.testng.asserts.SoftAssert;
 
 public abstract class SearchTestRunner extends EpizyUserTestRunner {
     SoftAssert softAssert = new SoftAssert();
-    static SortByFilter SELECT_SORT_BY_FILTER = SortByFilter.NAME_AZ;
-    static CountOfProducts SELECT_SHOW_FILTER = CountOfProducts.FIFTY;
 
     @BeforeMethod()
     public void searchInput() {
