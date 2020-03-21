be78532b28d50ee519ab6905310669928d54bbaf
51stRoR <51sttestinrror@gmail.com>
2020-03-21 20:16:38 +0200

    tabulation fix

--- src/test/java/com/softserve/edu/opencart/tools/RegularExpression.java
+++ src/test/java/com/softserve/edu/opencart/tools/RegularExpression.java
@@ -6,38 +6,39 @@ import java.util.regex.Pattern;
 
 public final class RegularExpression {
 
-  public RegularExpression() {
-  }
-
-  public int getNumberFromString(String str){
-    int stringNumber = 0;
-    Pattern regularExpressionPattern = Pattern.compile("-?\\d+");
-    Matcher matcher = regularExpressionPattern.matcher(str);
-    while (matcher.find()){
-      stringNumber = Integer.parseInt(matcher.group());
-    break;}
-    return stringNumber;
-  }
-
-  public BigDecimal getBigDecimalFromTheShoppingCartPriceField(String field){
-    String stringValue = "";
-    Pattern pattern = Pattern.compile("(\\d{1,3},)*\\d{1,3}\\.\\d{2}");
-    Matcher matcher = pattern.matcher(field);
-    while (matcher.find()){
-      stringValue = field.substring(matcher.start(), matcher.end());
-      stringValue = stringValue.replace(",", "");
-    }
-    return new BigDecimal(stringValue);
-  }
-
-  private String getClearCategoryName(String strInput){
-    String strResult = "";
-    Pattern p = Pattern.compile("[A-Z].+[^ (0-9)]");
-    Matcher m = p.matcher(strInput);
-    while(m.find()){
-      strResult = String.valueOf(m.group());
+    public RegularExpression() {
     }
 
-    return strResult;
-  }
+    public int getNumberFromString(String str) {
+        int stringNumber = 0;
+        Pattern regularExpressionPattern = Pattern.compile("-?\\d+");
+        Matcher matcher = regularExpressionPattern.matcher(str);
+        while (matcher.find()) {
+            stringNumber = Integer.parseInt(matcher.group());
+            break;
+        }
+        return stringNumber;
+    }
+
+    public BigDecimal getBigDecimalFromTheShoppingCartPriceField(String field) {
+        String stringValue = "";
+        Pattern pattern = Pattern.compile("(\\d{1,3},)*\\d{1,3}\\.\\d{2}");
+        Matcher matcher = pattern.matcher(field);
+        while (matcher.find()) {
+            stringValue = field.substring(matcher.start(), matcher.end());
+            stringValue = stringValue.replace(",", "");
+        }
+        return new BigDecimal(stringValue);
+    }
+
+    private String getClearCategoryName(String strInput) {
+        String strResult = "";
+        Pattern p = Pattern.compile("[A-Z].+[^ (0-9)]");
+        Matcher m = p.matcher(strInput);
+        while (m.find()) {
+            strResult = String.valueOf(m.group());
+        }
+
+        return strResult;
+    }
 }
