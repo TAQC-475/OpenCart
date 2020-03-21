3c863ef3a957705c21236c1d9fc6a71eaa6260f7
BorisTitarenko <specmohnat2309@gmail.com>
2020-03-21 21:30:41 +0200

    Add SuccessfulUpdatePasswordLoginPage

--- src/test/java/com/softserve/edu/opencart/data/AlertMessagesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.softserve.edu.opencart.data;
-
-public class AlertMessagesRepository {
-    public static String getUpdatePasswordSuccess(){
-        return "Success: Your password has been successfully updated.";
-    }
-}
