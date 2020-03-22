21f51d6e8b034613c906860e00402f33cfb85f76
stadnytskyivp <stadnytskyi.volodymyr@gmail.com>
2020-03-22 18:10:54 +0200

    add methods

--- src/test/java/com/softserve/edu/opencart/pages/user/common/MainMenuComponent.java
+++ src/test/java/com/softserve/edu/opencart/pages/user/common/MainMenuComponent.java
@@ -1,18 +1,18 @@
 package com.softserve.edu.opencart.pages.user.common;
-// added by Volodymyr
 
 import com.softserve.edu.opencart.data.MenuItems;
+import com.softserve.edu.opencart.tools.ErrorUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainMenuComponent {
 
     protected final String LIST_SUB_CATEGOIES_CSSSELECTOR = "div.dropdown-inner ul.list-unstyled li";
+    protected final String DROPDOWN_TOP_MENU_CSSSELECTOR = "#menu .dropdown-menu";
 
     private MenuItems categoryItem;
     private List<WebElement> menuItemList;
@@ -31,7 +31,7 @@ public class MainMenuComponent {
         // init elements
         menuItemList = driver.findElements(By.xpath("//ul[@class='nav navbar-nav']/li/a"));
 //        menuItemList = driver.findElements(By.xpath("//ul[@class='nav navbar-nav']//li/a"));
-        dropdownComponent = new DropdownComponent(driver, By.cssSelector(LIST_SUB_CATEGOIES_CSSSELECTOR));  // hardcode
+//        dropdownComponent = new DropdownComponent(driver, By.cssSelector(LIST_SUB_CATEGOIES_CSSSELECTOR));  // hardcode
 
     }
 
@@ -40,30 +40,63 @@ public class MainMenuComponent {
         return menuItemList;
     }
 
-    private Integer getMenuItemNumber(MenuItems categoryItem) {
-        return getMenuItemList().indexOf(categoryItem);
+    private DropdownComponent createDropdownComponent(By searchLocator) {
+        dropdownComponent = new DropdownComponent(driver, searchLocator);
+        return getDropdownComponent();
     }
 
-    public MainMenuComponent clickMenuItem(){
-        getMenuItemList().get(getMenuItemNumber(categoryItem)).click();
-        return new MainMenuComponent(driver);
+    private Integer getMenuItemNumber(MenuItems categoryItem) {
+        return getMenuItemList().indexOf(categoryItem);
     }
 
     public DropdownComponent getDropdownComponent() {
         return dropdownComponent;
     }
 
-    public void clickByPartialName(MenuItem menuItem) {
+    public WebElement getMenuTopByCategoryPartialName(String categoryName) {
+        WebElement result = null;
+        for (WebElement current : getMenuItemList()) {
+            if (current.getText().toLowerCase().contains(categoryName.toLowerCase())) {
+                result = current;
+                break;
+            }
+        }
+        return result;
     }
 
-    // Functional
-    public List<String> getMenuItemListText() {
+    public List<String> getMenuTopText() {
         List<String> result = new ArrayList<>();
         for (WebElement menuItem : getMenuItemList()) {
             result.add(menuItem.getText());
         }
         return result;
     }
+
+    public void clickMenuTopByCategoryPartialName(String categoryName) {
+        boolean isClickable = false;
+        for (String current : getMenuTopText()) {
+            if (current.toLowerCase().contains(categoryName.toLowerCase())) {
+                isClickable = true;
+                break;
+            }
+        }
+        ErrorUtils.createCustomException(!isClickable,
+                String.format("OPTION_NOT_FOUND_MESSAGE",
+                        categoryName, getMenuTopText().toString()));
+        getMenuTopByCategoryPartialName(categoryName).click();
+    }
+
+//    public void clickMenuTopByPartialName(String categoryName, String optionName) {
+//        System.out.println("categoryName : "+categoryName);
+//        clickMenuTopByCategoryPartialName(categoryName);
+//        System.out.println("optionName : "+optionName);
+//        createDropdownComponent(By.cssSelector(DROPDOWN_TOP_MENU_CSSSELECTOR));
+//        clickDropdownComponentByPartialName(optionName);
+//    }
+
+
+
+    // Functional
 
     // Business Logic
 
--- src/test/java/com/softserve/edu/opencart/tests/HorizontalVerticalMenuCompareTest.java
+++ src/test/java/com/softserve/edu/opencart/tests/HorizontalVerticalMenuCompareTest.java
@@ -12,18 +12,41 @@ import java.util.List;
 
 public class HorizontalVerticalMenuCompareTest extends EpizyUserTestRunner {
     @Test
-    public void menuTest() {
+    public void menuTest() throws InterruptedException {
+
+//        try {
+//            loadApplication().gotoProductsSidebarEmptyPage().myMethod1();//checkFullEmptyPage();
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+        //System.out.println(""+mainMenuItems1);
+
+//        for(WebElement w:loadApplication().getMainMenuComponent().getMenuItemList()){
+//
+//        }
+
+        loadApplication()
+                .getMainMenuComponent()
+                .clickMenuTopByCategoryPartialName("Desktops")
+        ;
+        Thread.sleep(1000);
+//        loadApplication().clickMenuTopByCategoryPartialName("Laptops");
+//        Thread.sleep(1000);
+//        loadApplication().clickMenuTopByPartialName("Desktops", "Mac (1)");
+//        Thread.sleep(1000);
 
         // reading mainMenuItems
-        List<String> mainMenuItems = loadApplication().getMainMenuComponent().getMenuItemListText();
-        System.out.println(mainMenuItems);
-
-        // reading leftProductSideBarItems
-        List<String> leftMenuItems = loadApplication().gotoProductsSidebarEmptyPage().getProductsSidebarEmptyPage().getLeftMenuItemListText();
-        System.out.println(leftMenuItems);
+//        System.out.println("Top menu");
+//        List<String> mainMenuItems = loadApplication().getMainMenuComponent().getMenuItemListText();
+//        System.out.println(mainMenuItems);
+//
+//        // reading leftProductSideBarItems
+//        System.out.println("Left menu");
+//        List<String> leftMenuItems = loadApplication().gotoProductsSidebarEmptyPage().getProductsSidebarEmptyPage().getLeftMenuItemListText();
+//        System.out.println(leftMenuItems);
 
         // assert
-        Assert.assertEquals(mainMenuItems.toString(), leftMenuItems.toString());
+//        Assert.assertEquals(mainMenuItems.toString(), leftMenuItems.toString());
     }
 
 }
