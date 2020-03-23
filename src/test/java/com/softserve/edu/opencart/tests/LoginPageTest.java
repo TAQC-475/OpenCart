53dc156a6c621e407156a9fcfa6bdafc9b91692e
BorisTitarenko <borys.tytarenko.pz.2017@lpnu.ua>
2020-03-23 20:41:46 +0200

    Add unseccessfulLoginTest

--- src/test/java/com/softserve/edu/opencart/tests/LoginPageTest.java
+++ src/test/java/com/softserve/edu/opencart/tests/LoginPageTest.java
@@ -3,6 +3,7 @@ package com.softserve.edu.opencart.tests;
 import com.softserve.edu.opencart.data.*;
 import com.softserve.edu.opencart.pages.user.account.MyAccountPage;
 import com.softserve.edu.opencart.pages.user.account.SuccessfulUpdatePasswordLoginPage;
+import com.softserve.edu.opencart.pages.user.account.UnsuccessfulLoginPage;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import static org.testng.Assert.*;
@@ -20,6 +21,13 @@ public class LoginPageTest extends LocalTestRunner {
     @DataProvider(name = "validUserDataProvider")
     public Object[] getValidUser(){
         return new Object[]{UserRepository.get().getDefault()};
+    }
+
+    @DataProvider(name = "invalidUserDataProvider")
+    public Object[][] invalidUsers() {
+        return new Object[][] {
+                { UserRepository.get().getInvalid() },
+        };
     }
 
     @Test(priority = 3, dataProvider = "emailClientDataProvider")
@@ -44,6 +52,19 @@ public class LoginPageTest extends LocalTestRunner {
 
         assertTrue(ApplicationStatus.get().isLogged());
     }
+
+    @Test(priority = 2, dataProvider = "invalidUserDataProvider")
+    public void unsuccessfulLoginTest(IUser invalidUser){
+        UnsuccessfulLoginPage loginPage = loadApplication()
+                .gotoLoginPage()
+                .unsuccessfulLoginPage(invalidUser);
+
+        assertFalse(ApplicationStatus.get().isLogged());
+        assertTrue(loginPage.getAlertWarningText()
+                .contains(UnsuccessfulLoginPage.EXPECTED_LOGIN_MESSAGE));
+    }
+
+
 
 
 }
