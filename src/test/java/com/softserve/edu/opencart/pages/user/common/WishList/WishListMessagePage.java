ca4ef03fe46c09de72c3830effb872b80baf8a68
Boris <borismenko.sgmail.com>
2020-03-23 23:24:35 +0200

    add Commetn to wishListTest

--- src/test/java/com/softserve/edu/opencart/pages/user/common/WishList/WishListMessagePage.java
+++ src/test/java/com/softserve/edu/opencart/pages/user/common/WishList/WishListMessagePage.java
@@ -21,6 +21,9 @@ public class WishListMessagePage extends WishListPage {
     initElements();
   }
 
+  /**
+   * This method initializations all elements on page.
+   */
   private void initElements() {
     removeMessage = driver.findElement(By.xpath("//div[@class = 'alert alert-success']"));
     addToCartMessage = driver.findElement(By.xpath("//div[@class = 'alert alert-success']"));
@@ -28,37 +31,56 @@ public class WishListMessagePage extends WishListPage {
     waitUtils = new WaitUtils(driver, 10);
   }
 
+
+  /**
+   * This method gets remove Alert message.
+   */
   public WebElement getRemoveMessage() {
     return removeMessage;
   }
 
+  /**
+   * This method gets add product to cart alert message.
+   */
   public WebElement getAddToCartMessage() {
     return addToCartMessage;
   }
 
+  /**
+   * This method gets cross button.
+   */
   public WebElement getCrossButton() {
     return crossButton;
   }
 
+  /**
+   * This method clicks on cross button.
+   */
+  public void clickCrossButton() {
+    getCrossButton().click();
+  }
+
+  /**
+   * This method gets remove alert message text.
+   */
   public String getRemoveMessageText() {
     waitUtils.waitForAlertVisibility();
     return getRemoveMessage().getText();
   }
 
+  /**
+   * This method gets add to cart message text.
+   */
   public String getAddToCartMessageText() {
     return getAddToCartMessage().getText();
   }
 
-  public void clickCrossButton() {
-    getCrossButton().click();
-  }
-
-
+  /**
+   * This method clicks on cross button.
+   */
   public WishListPage clickOnCrossButton() {
     clickCrossButton();
     return new WishListPage(driver);
   }
-
-
 
 }
--- src/test/java/com/softserve/edu/opencart/pages/user/common/WishList/WishListProductComponent.java
+++ src/test/java/com/softserve/edu/opencart/pages/user/common/WishList/WishListProductComponent.java
@@ -5,6 +5,9 @@ import java.math.BigDecimal;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
+/**
+ *  Wish List Component class.
+ */
 public final class WishListProductComponent {
 
   private WebElement tableLayout;
@@ -21,7 +24,9 @@ public final class WishListProductComponent {
     this.tableLayout = tableLayout;
     initElements();
   }
-
+  /**
+   * This method initializations all elements which product in wish list has.
+   */
   private void initElements() {
     image = tableLayout
         .findElement(By.xpath("./td[@class='text-center']/a"));
@@ -34,59 +39,111 @@ public final class WishListProductComponent {
         .findElement(By.xpath(".//button[@data-original-title='Add to Cart']"));
     removeFromWIshListButton = tableLayout
         .findElement(By.xpath(".//a[@data-original-title='Remove']"));
-
   }
 
+  // Page Object
+
+  /**
+   * This method gets tableLayout.
+   */
+  public WebElement getTableLayout() {
+    return tableLayout;
+  }
+
+  /**
+   * This method gets image.
+   */
   public WebElement getImage() {
     return image;
   }
 
+  /**
+   * This method gets productName.
+   */
   public WebElement getName() {
     return name;
   }
 
+  /**
+   * This method clicks on product name.
+   */
   public void clickProductName(){
     getName().click();
   }
 
+  /**
+   * This method gets getNameText.
+   */
   public String getNameText() {
-
     return getName().getText();
   }
 
+  /**
+   * This method gets product model.
+   */
   public WebElement getModel() {
     return model;
   }
 
+  /**
+   * This method gets product model text.
+   */
   public String getModelText(){
      return getModel().getText();
   }
 
+  /**
+   * This method gets product stock.
+   */
   public WebElement getStock() {
     return stock;
   }
 
+  /**
+   * This method gets product stock text.
+   */
+  public String getStockText() {
+    return getStock().getText();
+  }
+
+  /**
+   * This method gets product price.
+   */
   public WebElement getPrice() {
     return price;
   }
 
+  /**
+   * This method gets product price text.
+   */
   public BigDecimal getPriceText(){
     return new RegularExpression().getBigDecimalFromTheShoppingCartField(getPrice().getText());
   }
 
-
+  /**
+   * This method gets add to cart button.
+   */
   public WebElement getAddToCartButton() {
     return addToCartButton;
   }
 
+  /**
+   * This method clicks add to cart button.
+   */
   public void clickAddToCartButton() {
     getAddToCartButton().click();
   }
 
+  /**
+   * This method gets remove button.
+   */
   public WebElement getRemoveFromWIshListButton() {
     return removeFromWIshListButton;
   }
 
+  /**
+   * This method clicks remove button.
+   */
   public void clickRemoveFromWishListButton() {
     getRemoveFromWIshListButton().click();
   }
