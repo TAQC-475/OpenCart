ee6d4b3da7169856650df2143cbc3fdd7e0dcee1
Andrew Rorat <rorat6937@gmail.com>
2020-03-24 18:22:58 +0200

    Add new dataProvider to DataForAdminTest

--- src/test/java/com/softserve/edu/opencart/tests/SearchSortingTest.java
+++ src/test/java/com/softserve/edu/opencart/tests/SearchSortingTest.java
@@ -3,36 +3,13 @@ package com.softserve.edu.opencart.tests;
 import com.softserve.edu.opencart.data.CountOfProducts;
 import com.softserve.edu.opencart.data.Pagination;
 import com.softserve.edu.opencart.data.SortByFilter;
+import com.softserve.edu.opencart.data.data_provider_repository.DataForSearchTests;
 import org.testng.Assert;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-
-import java.lang.reflect.Method;
 
 public class SearchSortingTest extends SearchTestRunner {
 
-    //TODO error messages
-
-    @DataProvider (name = "dataForSortSearchingTests")
-    private Object[][] dataForSortSearchingTests(Method method) {
-        String testCase = method.getName();
-        if ("checkSortByDropDownMenu".equalsIgnoreCase(testCase)){
-        return new Object[][]{{
-                SortByFilter.MODEL_AZ,
-                SortByFilter.MODEL_AZ}};}
-        else if ("checkShowDropDownMenu".equalsIgnoreCase(testCase)){
-            return new Object[][]{{
-                    CountOfProducts.FIFTY,
-                    CountOfProducts.FIFTY}};
-        }else if ("checkPagination".equalsIgnoreCase(testCase)) {
-            return new Object[][]{{Pagination.NEXT_PAGE, "2"}};
-        }
-        else {
-            return new Object[][]{{ "ERROR: Data Provider can't find method: " + testCase}};
-        }
-    }
-
-    @Test(dataProvider = "dataForSortSearchingTests",
+    @Test(dataProvider = "dataForSortSearchingTests", dataProviderClass = DataForSearchTests.class,
             description = "verify 'Sort by:' drop down filter")
     public void checkSortByDropDownMenu(SortByFilter actualFilter, SortByFilter expectedFilter) {
         successPage()
@@ -42,7 +19,8 @@ public class SearchSortingTest extends SearchTestRunner {
                 String.format("Expect: %s, but found: %s", actualFilter, expectedFilter));
     }
 
-    @Test(dataProvider = "dataForSortSearchingTests", description = "verify 'Show:' drop down filter")
+    @Test(dataProvider = "dataForSortSearchingTests", dataProviderClass = DataForSearchTests.class,
+            description = "verify 'Show:' drop down filter")
     public void checkShowDropDownMenu(CountOfProducts actualInput, CountOfProducts expectedInput) {
         successPage()
                 .showProductsByCount(actualInput);
@@ -63,7 +41,8 @@ public class SearchSortingTest extends SearchTestRunner {
         softAssert.assertAll();
     }
 
-    @Test(dataProvider = "dataForSortSearchingTests", description = "verify pagination on 'Search' page")
+    @Test(dataProvider = "dataForSortSearchingTests", dataProviderClass = DataForSearchTests.class,
+            description = "verify pagination on 'Search' page")
     public void checkPagination(Pagination actualPage, String expectedPage) {
         successPage()
                 .clickNeededPage(actualPage);
