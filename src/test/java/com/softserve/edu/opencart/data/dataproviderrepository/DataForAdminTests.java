53f62c7dde00db7853ec751b8401a0880d2c3449
stadnytskyivp <stadnytskyi.volodymyr@gmail.com>
2020-03-24 15:53:45 +0200

    modifyed admin tests

--- src/test/java/com/softserve/edu/opencart/data/dataproviderrepository/DataForAdminTests.java
+++ src/test/java/com/softserve/edu/opencart/data/dataproviderrepository/DataForAdminTests.java
@@ -1,14 +1,15 @@
 package com.softserve.edu.opencart.data.dataproviderrepository;
 
 import com.softserve.edu.opencart.data.UserRepository;
+import com.softserve.edu.opencart.data.categories.MenuCategoryRepository;
 import com.softserve.edu.opencart.data.creationProductAdminPanel.NewProductRepository;
 import org.testng.annotations.DataProvider;
 
 import java.lang.reflect.Method;
 
 public class DataForAdminTests {
-    @DataProvider(name = "addRouterFromAdmin")
-    public Object[][] ProductsData(Method method) {
+    @DataProvider
+    public Object[][] addRouterProduct(Method method) {
         return new Object[][]{
                 {UserRepository.get().getAdmin(),
                         NewProductRepository.router().getProductName(),
@@ -16,6 +17,16 @@ public class DataForAdminTests {
                         NewProductRepository.router().getModel(),
                         NewProductRepository.router().getPrice(),
                         NewProductRepository.router().getCategory()}
+        };
+    }
+
+    @DataProvider
+    public Object[][] addRoutersCategory(Method method) {
+        return new Object[][]{
+                {UserRepository.get().getAdmin(),
+                        MenuCategoryRepository.routers().getName(),
+                        MenuCategoryRepository.routers().getTitle(),
+                        MenuCategoryRepository.routers().getParent()}
         };
     }
 
