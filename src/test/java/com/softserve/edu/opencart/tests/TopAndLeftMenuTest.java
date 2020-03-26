31461f724620f3343ece5fe99f20e49d2214361e
stadnytskyivp <stadnytskyi.volodymyr@gmail.com>
2020-03-26 11:19:34 +0200

    adding javadoc to tests

--- src/test/java/com/softserve/edu/opencart/tests/TopAndLeftMenuTest.java
+++ src/test/java/com/softserve/edu/opencart/tests/TopAndLeftMenuTest.java
@@ -1,66 +1,22 @@
 package com.softserve.edu.opencart.tests;
 
+import com.softserve.edu.opencart.pages.user.common.MainMenuComponent;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.util.List;
-import java.util.Map;
-
 public class TopAndLeftMenuTest extends LocalTestRunner {
+
+    /**
+     * loading application, go to top menu on home page, getting all top menu category names, going to left menu,
+     * getting all left menu categories, verifying that the menu categories match
+     */
     @Test
-    public void menuTest() throws InterruptedException {
+    public void menuTest() {
 
-//        Map<String, List<String>> topMenuCategories = loadApplication()
-//                .gotoMainMenuComponent()
-//                .getMenuCategoriesMap();
+        MainMenuComponent menuComponent = loadApplication().gotoMainMenuComponent();
 
-        Map<String, List<String>> leftMenuItems = loadApplication()
-                .gotoMainMenuComponent()
-                .gotoEmptyLeftMenu()
-                .getLeftMenuCategoriesMap();
-
-        System.out.println(leftMenuItems);
-
-//        System.out.println(topMenuCategories);
-//        Assert.assertEquals(topMenuCategories, topMenuCategories);
-        Assert.assertEquals(leftMenuItems, leftMenuItems);
-
-
-//        MainMenuComponent menuComponent = loadApplication().gotoMainMenuComponent()
-//                .chooseCategory(Categories.DESKTOPS);
-//
-//        String topCategory = menuComponent.getAllCategories();
-//
-//        List<String> leftMenu = menuComponent
-//                .gotoFullLeftMenu()
-//                .getLeftMenuItemListText();
-//
-//        System.out.println(leftMenu.toString());
-//
-//        System.out.println("*** : "+topMenuCategories);
-//        Assert.assertEquals(topMenuCategories,topMenuCategories);
-
-
-//        loadApplication().clickMenuTopByCategoryPartialName("Laptops");
-//        Thread.sleep(1000);
-//        loadApplication().clickMenuTopByPartialName("Desktops", "Mac (1)");
-//        Thread.sleep(1000);
-
-        // reading mainMenuItems
-//        System.out.println("Top menu");
-//        List<String> mainMenuItems = loadApplication()
-//                .getMainMenuComponent()
-//                .getMenuTopText();
-//        System.out.println(mainMenuItems);
-//
-//        // reading leftProductSideBarItems
-//        System.out.println("Left menu");
-//        List<String> leftMenuItems = loadApplication()
-//                .gotoProductsSidebarEmptyPage()
-//                .getProductsSidebarEmptyPage()
-//                .getLeftMenuItemListText();
-//        System.out.println(leftMenuItems);
-
+        Assert.assertEquals(menuComponent.getMenuCategoriesMap(),
+                menuComponent.gotoEmptyLeftMenu().getLeftMenuCategoriesMap());
     }
 
 }
